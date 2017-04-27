9e5198e08dab6e308e01754a6c1e0b9be4270b00
Alexandre Dias <alexandrermd@gmail.com>
2017-04-27 02:39:30 -0400

    Adding tempo de formacao

--- course_pattern.py
+++ course_pattern.py
@@ -58,7 +58,7 @@ for line in file:
 		if (admission in listAdmission):
 			listAdmission[admission]['ingressantes'][sexo] += 1;
 		else:
-			listAdmission[admission] = { 'ingressantes' : { 'M': 0, 'F': 0, 'idade': [] }, 'semevasao' : { 'M': 0, 'F': 0, 'idade': [] }, 'desistente' : { 'M': 0, 'F': 0, 'idade': [] }, 'cancelamento' : { 'M': 0, 'F': 0, 'idade': [] }, 'formado' : { 'M': 0, 'F': 0, 'idade': [] }, 'jubilado 01' : { 'M': 0, 'F': 0, 'idade': [] }, 'jubilado 02' : { 'M': 0, 'F': 0, 'idade': [] }, 'transferido' : { 'M': 0, 'F': 0, 'idade': [] }, 'excluido' : { 'M': 0, 'F': 0, 'idade': [] } };
+			listAdmission[admission] = { 'ingressantes' : { 'M': 0, 'F': 0, 'idade': [] }, 'semevasao' : { 'M': 0, 'F': 0, 'idade': [] }, 'desistente' : { 'M': 0, 'F': 0, 'idade': [] }, 'cancelamento' : { 'M': 0, 'F': 0, 'idade': [] }, 'formado' : { 'M': 0, 'F': 0, 'idade': [], 'tempo' : [] }, 'jubilado 01' : { 'M': 0, 'F': 0, 'idade': [] }, 'jubilado 02' : { 'M': 0, 'F': 0, 'idade': [] }, 'transferido' : { 'M': 0, 'F': 0, 'idade': [] }, 'excluido' : { 'M': 0, 'F': 0, 'idade': [] } };
 			listAdmission[admission]['ingressantes'][sexo] = 1;
 
 		listAdmission[admission]['ingressantes']['idade'].append(idade);
@@ -66,10 +66,17 @@ for line in file:
 		if (reason in dictStatus) :
 			listAdmission[admission][dictStatus[reason]][sexo] +=  1;
 
+			if (dictStatus[reason] is 'formado' and len(evasao) > 0): 
+				anoegresso = datetime.strptime(evasao, '%d/%m/%Y');
+				anoingresso = datetime.strptime(stringresso, '%d/%m/%Y');
+				formacao = calculaIdade(anoingresso, anoegresso);
+
+				listAdmission[admission][dictStatus[reason]]['tempo'].append(formacao);
+
 		if (reason in dictStatusWhiteList and len(separateFields[11]) > 0):
 			anoegresso = datetime.strptime(evasao, '%d/%m/%Y');
 			aniversario = datetime.strptime(nascimento, '%d/%m/%Y');
-			idade = calculaIdade(aniversario, anoegresso);			
+			idade = calculaIdade(aniversario, anoegresso);
 			listAdmission[admission][dictStatus[reason]]['idade'].append(idade);
 
 for admission in listAdmission: 
@@ -86,22 +93,23 @@ for admission in listAdmission:
 
 	print "\nHomens: %i" % (listAdmission[admission]['ingressantes']['M']);	
 	print "Mulheres: %i" % (listAdmission[admission]['ingressantes']['F']);	
-	print "Media idade: %.0f anos" % (sum(listAdmission[admission]['ingressantes']['idade']) / ingressantes);
+	print "Media idade: %.2f anos" % (sum(listAdmission[admission]['ingressantes']['idade']) / float(ingressantes));
 
 	# Formados
 	formado = listAdmission[admission]['formado']['M'] + listAdmission[admission]['formado']['F'];
 
 	if (formado > 0):
 		print "\nFormado/Ingresso (%i):  %.2f" % (formado, (formado * 100)/ ingressantes);	
-		print "Media idade: %.0f anos" % (sum(listAdmission[admission]['formado']['idade']) / formado);
+		print "Media idade: %.0f anos" % (sum(listAdmission[admission]['formado']['idade']) / float(formado));
+		print "Media tempo formacao: %.2f anos" % (sum(listAdmission[admission]['formado']['tempo']) / float(len(listAdmission[admission]['formado']['tempo'])));
 	
 		if (listAdmission[admission]['formado']['M'] > 0) :
-			print "Formado/Ingresso H (%i):  %.2f" % (listAdmission[admission]['formado']['M'], (listAdmission[admission]['formado']['M'] * 100)/ ingressantes);
+			print "Formado/Ingresso H (%i):  %.2f" % (listAdmission[admission]['formado']['M'], (listAdmission[admission]['formado']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Formado/Ingresso H: 0";
 
 		if (listAdmission[admission]['formado']['F'] > 0) :
-			print "Formado/Ingresso M (%i):  %.2f" % (listAdmission[admission]['formado']['F'], (listAdmission[admission]['formado']['F'] * 100)/ ingressantes);
+			print "Formado/Ingresso M (%i):  %.2f" % (listAdmission[admission]['formado']['F'], (listAdmission[admission]['formado']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Formado/Ingresso M: 0";
 
@@ -110,15 +118,15 @@ for admission in listAdmission:
 	semevasao = listAdmission[admission]['semevasao']['M'] + listAdmission[admission]['semevasao']['F'];
 
 	if (semevasao > 0):
-		print "\nSem Evasao/Ingresso (%i):  %.2f" % (semevasao, (semevasao * 100)/ ingressantes);
+		print "\nSem Evasao/Ingresso (%i):  %.2f" % (semevasao, (semevasao * 100)/ float(ingressantes));
 
 		if (listAdmission[admission]['semevasao']['M'] > 0) :
-			print "Sem Evasao/Ingresso H (%i):  %.2f" % (listAdmission[admission]['semevasao']['M'], (listAdmission[admission]['semevasao']['M'] * 100)/ ingressantes);
+			print "Sem Evasao/Ingresso H (%i):  %.2f" % (listAdmission[admission]['semevasao']['M'], (listAdmission[admission]['semevasao']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Sem Evasao/Ingresso H: 0";
 
 		if (listAdmission[admission]['semevasao']['F'] > 0) :
-			print "Sem Evasao/Ingresso M (%i):  %.2f" % (listAdmission[admission]['semevasao']['F'], (listAdmission[admission]['semevasao']['F'] * 100)/ ingressantes);
+			print "Sem Evasao/Ingresso M (%i):  %.2f" % (listAdmission[admission]['semevasao']['F'], (listAdmission[admission]['semevasao']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Sem Evasao/Ingresso M: 0";
 
@@ -127,20 +135,20 @@ for admission in listAdmission:
 	jubilados1 = listAdmission[admission]['jubilado 01']['M'] + listAdmission[admission]['jubilado 01']['F'];
 
 	if (jubilados1 > 0):
-		print "\nJubilado 01/Ingresso (%i):  %.2f" % (jubilados1, (jubilados1 * 100)/ ingressantes);
+		print "\nJubilado 01/Ingresso (%i):  %.2f" % (jubilados1, (jubilados1 * 100)/ float(ingressantes));
 
 		if (jubilados1 > 1):
-			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['jubilado 01']['idade']) / jubilados1);
+			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['jubilado 01']['idade']) / float(jubilados1));
 			print "Mais velho: %.0f anos" % (max(listAdmission[admission]['jubilado 01']['idade']));
 			print "Mais novo: %.0f anos" % (min(listAdmission[admission]['jubilado 01']['idade']));				
 	
 		if (listAdmission[admission]['jubilado 01']['M'] > 0) :
-			print "Jubilado 01/Ingresso H (%i):  %.2f" % (listAdmission[admission]['jubilado 01']['M'], (listAdmission[admission]['jubilado 01']['M'] * 100)/ ingressantes);
+			print "Jubilado 01/Ingresso H (%i):  %.2f" % (listAdmission[admission]['jubilado 01']['M'], (listAdmission[admission]['jubilado 01']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Jubilado 01/Ingresso H: 0";		
 
 		if (listAdmission[admission]['jubilado 01']['F'] > 0) :
-			print "Jubilado 01/Ingresso M (%i):  %.2f" % (listAdmission[admission]['jubilado 01']['F'], (listAdmission[admission]['jubilado 01']['F'] * 100)/ ingressantes);
+			print "Jubilado 01/Ingresso M (%i):  %.2f" % (listAdmission[admission]['jubilado 01']['F'], (listAdmission[admission]['jubilado 01']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Jubilado 01/Ingresso M: 0";	
 
@@ -149,20 +157,20 @@ for admission in listAdmission:
 	jubilados2 = listAdmission[admission]['jubilado 02']['M'] + listAdmission[admission]['jubilado 02']['F'];
 
 	if (jubilados2 > 0):
-		print "\nJubilado 02/Ingresso (%i):  %.2f" % (jubilados2, (jubilados2 * 100)/ ingressantes);
+		print "\nJubilado 02/Ingresso (%i):  %.2f" % (jubilados2, (jubilados2 * 100)/ float(ingressantes));
 
 		if (jubilados2 > 1):
-			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['jubilado 02']['idade']) / jubilados2);
+			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['jubilado 02']['idade']) / float(jubilados2));
 			print "Mais velho: %.0f anos" % (max(listAdmission[admission]['jubilado 02']['idade']));
 			print "Mais novo: %.0f anos" % (min(listAdmission[admission]['jubilado 02']['idade']));		
 
 		if (listAdmission[admission]['jubilado 02']['M'] > 0) :
-			print "Jubilado 02/Ingresso H (%i):  %.2f" % (listAdmission[admission]['jubilado 02']['M'], (listAdmission[admission]['jubilado 02']['M'] * 100)/ ingressantes);
+			print "Jubilado 02/Ingresso H (%i):  %.2f" % (listAdmission[admission]['jubilado 02']['M'], (listAdmission[admission]['jubilado 02']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Jubilado 02/Ingresso H: 0";		
 
 		if (listAdmission[admission]['jubilado 01']['F'] > 0) :
-			print "Jubilado 02/Ingresso M (%i):  %.2f" % (listAdmission[admission]['jubilado 02']['F'], (listAdmission[admission]['jubilado 02']['F'] * 100)/ ingressantes);
+			print "Jubilado 02/Ingresso M (%i):  %.2f" % (listAdmission[admission]['jubilado 02']['F'], (listAdmission[admission]['jubilado 02']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Jubilado 02/Ingresso M: 0";	
 
@@ -170,20 +178,20 @@ for admission in listAdmission:
 	excluido = listAdmission[admission]['excluido']['M'] + listAdmission[admission]['excluido']['F'];
 
 	if (excluido > 0):
-		print "\nExcluido/Ingresso (%i):  %.2f" % (excluido, (excluido * 100)/ ingressantes);
+		print "\nExcluido/Ingresso (%i):  %.2f" % (excluido, (excluido * 100)/ float(ingressantes));
 
 		if (excluido > 1):
-			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['excluido']['idade']) / excluido);
+			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['excluido']['idade']) / float(excluido));
 			print "Mais velho: %.0f anos" % (max(listAdmission[admission]['excluido']['idade']));
 			print "Mais novo: %.0f anos" % (min(listAdmission[admission]['excluido']['idade']));		
 
 		if (listAdmission[admission]['excluido']['M'] > 0) :
-			print "Excluido/Ingresso H (%i):  %.2f" % (listAdmission[admission]['excluido']['M'], (listAdmission[admission]['excluido']['M'] * 100)/ ingressantes);
+			print "Excluido/Ingresso H (%i):  %.2f" % (listAdmission[admission]['excluido']['M'], (listAdmission[admission]['excluido']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Excluido/Ingresso H: 0";		
 
 		if (listAdmission[admission]['excluido']['F'] > 0) :
-			print "Excluido/Ingresso M (%i):  %.2f" % (listAdmission[admission]['excluido']['F'], (listAdmission[admission]['excluido']['F'] * 100)/ ingressantes);
+			print "Excluido/Ingresso M (%i):  %.2f" % (listAdmission[admission]['excluido']['F'], (listAdmission[admission]['excluido']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Excluido/Ingresso M: 0";				
 
@@ -191,20 +199,20 @@ for admission in listAdmission:
 	desistente = listAdmission[admission]['desistente']['M'] + listAdmission[admission]['desistente']['F'];
 
 	if (desistente > 0):
-		print "\nDesistente/Ingresso (%i):  %.2f" % (desistente, (desistente * 100)/ ingressantes);			
+		print "\nDesistente/Ingresso (%i):  %.2f" % (desistente, (desistente * 100)/ float(ingressantes));			
 
 		if (desistente > 1):
-			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['desistente']['idade']) / desistente);
+			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['desistente']['idade']) / float(desistente));
 			print "Mais velho: %.0f anos" % (max(listAdmission[admission]['desistente']['idade']));
 			print "Mais novo: %.0f anos" % (min(listAdmission[admission]['desistente']['idade']));
 		
 		if (listAdmission[admission]['desistente']['M'] > 0) :
-			print "Desistente/Ingresso H (%i):  %.2f" % (listAdmission[admission]['desistente']['M'], (listAdmission[admission]['desistente']['M'] * 100)/ ingressantes);
+			print "Desistente/Ingresso H (%i):  %.2f" % (listAdmission[admission]['desistente']['M'], (listAdmission[admission]['desistente']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Desistente/Ingresso H: 0";		
 
 		if (listAdmission[admission]['desistente']['F'] > 0) :
-			print "Desistente/Ingresso M (%i):  %.2f" % (listAdmission[admission]['desistente']['F'], (listAdmission[admission]['desistente']['F'] * 100)/ ingressantes);
+			print "Desistente/Ingresso M (%i):  %.2f" % (listAdmission[admission]['desistente']['F'], (listAdmission[admission]['desistente']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Desistente/Ingresso M: 0";		
 
@@ -213,20 +221,20 @@ for admission in listAdmission:
 	transferido = listAdmission[admission]['transferido']['M'] + listAdmission[admission]['transferido']['F'];
 
 	if (transferido > 0):
-		print "\nTransferido/Ingresso (%i):  %.2f" % (transferido, (transferido * 100)/ ingressantes);	
+		print "\nTransferido/Ingresso (%i):  %.2f" % (transferido, (transferido * 100)/ float(ingressantes));	
 
 		if (transferido > 1):
-			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['transferido']['idade']) / transferido);
+			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['transferido']['idade']) / float(transferido));
 			print "Mais velho: %.0f anos" % (max(listAdmission[admission]['transferido']['idade']));
 			print "Mais novo: %.0f anos" % (min(listAdmission[admission]['transferido']['idade']));		
 		
 		if (listAdmission[admission]['transferido']['M'] > 0) :
-			print "Transferido/Ingresso H (%i):  %.2f" % (listAdmission[admission]['transferido']['M'], (listAdmission[admission]['transferido']['M'] * 100)/ ingressantes);
+			print "Transferido/Ingresso H (%i):  %.2f" % (listAdmission[admission]['transferido']['M'], (listAdmission[admission]['transferido']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Transferido/Ingresso H: 0";		
 
 		if (listAdmission[admission]['transferido']['F'] > 0) :
-			print "Transferido/Ingresso M (%i):  %.2f" % (listAdmission[admission]['transferido']['F'], (listAdmission[admission]['transferido']['F'] * 100)/ ingressantes);
+			print "Transferido/Ingresso M (%i):  %.2f" % (listAdmission[admission]['transferido']['F'], (listAdmission[admission]['transferido']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Transferido/Ingresso M: 0";	
 	
@@ -235,20 +243,20 @@ for admission in listAdmission:
 	cancelamentos = listAdmission[admission]['cancelamento']['M'] + listAdmission[admission]['cancelamento']['F'];
 
 	if (cancelamentos > 0):
-		print "\nCancelamento/Ingresso (%i):  %.2f" % (cancelamentos, (cancelamentos * 100)/ ingressantes);	
+		print "\nCancelamento/Ingresso (%i):  %.2f" % (cancelamentos, (cancelamentos * 100)/ float(ingressantes));	
 
 		if (cancelamentos > 1):
-			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['cancelamento']['idade']) / cancelamentos);
+			print "Media idade: %.0f anos" % (sum(listAdmission[admission]['cancelamento']['idade']) / float(cancelamentos));
 			print "Mais velho: %.0f anos" % (max(listAdmission[admission]['cancelamento']['idade']));
 			print "Mais novo: %.0f anos" % (min(listAdmission[admission]['cancelamento']['idade']));				
 
 		if (listAdmission[admission]['cancelamento']['M'] > 0) :
-			print "Cancelamento/Ingresso H (%i):  %.2f" % (listAdmission[admission]['cancelamento']['M'], (listAdmission[admission]['cancelamento']['M'] * 100)/ ingressantes);
+			print "Cancelamento/Ingresso H (%i):  %.2f" % (listAdmission[admission]['cancelamento']['M'], (listAdmission[admission]['cancelamento']['M'] * 100)/ float(ingressantes));
 		else: 
 			print "Cancelamento/Ingresso H: 0";		
 
 		if (listAdmission[admission]['transferido']['F'] > 0) :
-			print "Cancelamento/Ingresso M (%i):  %.2f" % (listAdmission[admission]['cancelamento']['F'], (listAdmission[admission]['cancelamento']['F'] * 100)/ ingressantes);
+			print "Cancelamento/Ingresso M (%i):  %.2f" % (listAdmission[admission]['cancelamento']['F'], (listAdmission[admission]['cancelamento']['F'] * 100)/ float(ingressantes));
 		else: 
 			print "Cancelamento/Ingresso M: 0";	
 
